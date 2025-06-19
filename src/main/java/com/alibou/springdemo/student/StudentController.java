1dcb3e54c4631955b6632807009bd7636ea6a240
Yura Pokrovskiy <y-pokrovsky@yandex.ru>
2025-06-19 18:40:59 +0300

    implementing dao layer

--- src/main/java/com/alibou/springdemo/student/StudentController.java
+++ src/main/java/com/alibou/springdemo/student/StudentController.java
@@ -1,8 +1,6 @@
 package com.alibou.springdemo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +14,37 @@ public class StudentController {
         this.service = service;
     }
 
+    @PostMapping
+    public Student save(
+            @RequestBody Student student
+    ) {
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(
+            @PathVariable("email") String em
+    ) {
+        return service.findByEmail(em);
+    }
+
     @GetMapping
     public List<Student> findAllStudents() {
         return service.findAllStudents();
     }
 
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ) {
+        return service.update(student);
+
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(
+            @PathVariable("email") String email
+    ) {
+        service.delete(email);
+    }
 }
--- src/main/java/com/alibou/springdemo/student/StudentService.java
+++ src/main/java/com/alibou/springdemo/student/StudentService.java
@@ -1,28 +1,11 @@
 package com.alibou.springdemo.student;
 
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Ali",
-                        "Bouali",
-                        LocalDate.now(),
-                        "contact@aliboucoding.com",
-                        34
-                ),
-                new Student(
-                        "Student 3",
-                        "Bouali",
-                        LocalDate.now(),
-                        "contact@aliboucoding.com",
-                        28
-                )
-        );
-    }
+public interface StudentService {
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 }
